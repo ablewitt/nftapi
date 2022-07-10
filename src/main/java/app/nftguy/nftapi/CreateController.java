388d54187abaaeace7027812bc8c7905685b674f
ablewitt <4407104+ablewitt@users.noreply.github.com>
2022-07-10 10:34:33 +1000

    Enable cross origin support

--- src/main/java/app/nftguy/nftapi/CreateController.java
+++ src/main/java/app/nftguy/nftapi/CreateController.java
@@ -4,13 +4,12 @@ import app.nftguy.nftapi.model.NftTransactionDraft;
 import com.bloxbean.cardano.client.api.exception.ApiException;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class CreateController {
 
